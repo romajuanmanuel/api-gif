09f1dda160d43652606268004dd5d76bcd4b9f1b
Romaxolote <romajuanmanuel@gmail.com>
2025-07-02 13:35:38 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,24 @@
+// api.js
+export function api(query) {
+    const container = document.createElement('div');
+    container.id="wheaterContainer"
+    if (!query || query.trim() === '') {
+        query = 'cat';
+        alert("No has ingresado nada, te mostrare un gato");
+    }
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=D1ytZSOv9bk0ZHwtOS4oNQb5cdrY9B7b&s=${encodeURIComponent(query)}`, { mode: 'cors' })
+        .then(response => response.json())
+        .then(data => {
+            const imgUrl = data.data.images.original.url;
+
+            container.innerHTML = `
+                <h2>GIF</h2>
+                <img src="${imgUrl}" alt="GIF de Giphy" />
+            `;
+        })
+        .catch(err => {
+            container.textContent = 'Error al cargar el gif.';
+        });
+
+    return container;
+}
---
+++ src/loadPage.js
@@ -0,0 +1,37 @@
+//loadPage.js
+import { api } from './api.js';
+export function loadPage() {
+    const app = document.getElementById('app');
+
+    const header = document.createElement('header');
+    header.innerHTML = `<h1>Wheater App</h1>`;
+
+    const main = document.createElement('main');
+    main.id = 'main';
+
+    const footer = document.createElement('footer');
+    footer.innerHTML = `<small>&copy; 2025 Mi Sitio</small>`;
+
+    const inputBar = document.createElement('input');
+    inputBar.id = 'inputBar';
+    inputBar.placeholder = 'cat';
+
+    const searchBtn = document.createElement('button');
+    searchBtn.id = 'searchBtn';
+    searchBtn.textContent = "BUSCAR";
+
+    const resultContainer = document.createElement('div');
+    resultContainer.id = 'resultContainer';
+
+    searchBtn.addEventListener("click", () => {
+        const valor = inputBar.value;
+        resultContainer.innerHTML = '';
+        resultContainer.appendChild(api(valor));
+    });
+
+    resultContainer.appendChild(api("cat"));
+
+    main.append(inputBar, searchBtn, resultContainer);
+    app.append(header, main, footer);
+
+}
