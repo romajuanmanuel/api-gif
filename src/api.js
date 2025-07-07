ce468572787c5237015387eff0af7ccf51b294b0
Romaxolote <romajuanmanuel@gmail.com>
2025-07-07 14:25:19 -0300

    feat: refactor api to async function

--- src/api.js
+++ src/api.js
@@ -1,23 +1,27 @@
 // api.js
 export function api(query) {
     const container = document.createElement('div');
-    container.id="wheaterContainer"
+    container.id="gifContainer"
     if (!query || query.trim() === '') {
         query = 'cat';
         alert("No has ingresado nada, te mostrare un gato");
     }
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=D1ytZSOv9bk0ZHwtOS4oNQb5cdrY9B7b&s=${encodeURIComponent(query)}`, { mode: 'cors' })
-        .then(response => response.json())
-        .then(data => {
-            const imgUrl = data.data.images.original.url;
-
-            container.innerHTML = `
-                <img src="${imgUrl}" alt="GIF de Giphy" />
-            `;
-        })
-        .catch(err => {
-            container.textContent = 'Error al cargar el gif.';
-        });
+    getCats();
 
     return container;
+
+    async function getCats() {
+         await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=D1ytZSOv9bk0ZHwtOS4oNQb5cdrY9B7b&s=${encodeURIComponent(query)}`, { mode: 'cors' })
+            .then(response => response.json())
+            .then(data => {
+                const imgUrl = data.data.images.original.url;
+
+                container.innerHTML = `
+                <img src="${imgUrl}" alt="GIF de Giphy" />
+            `;
+            })
+            .catch(err => {
+                container.textContent = 'Error al cargar el gif.';
+            });
+    }
 }
